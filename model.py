b6efa0e0d8ef1096d2842ffc86fa32a00c0da308
a-bl <75131311+a-bl@users.noreply.github.com>
2021-06-18 14:25:39 +0200

    Update and rename task_2.py to model.py

--- model.py
+++ model.py
@@ -16,24 +16,6 @@ from keras.datasets import mnist
 print("x_train shape:", x_train.shape)
 print("x_test shape", x_test.shape)
 
-# Understanding and visualizing our data
-# understanding and visualizing our data
-# The MNIST database (Modified National Institute of Standards and Technology database) is a large database
-# of handwritten digits that is commonly used for training various image processing systems. The database is
-# also widely used for training and testing in the field of machine learning. The MNIST database contains
-# 60,000 training images and 10,000 testing images, with their correspoinding labels. These images are
-# grayscale image of 28 by 28 pixels.
-
-plt.figure(figsize=(8, 8))
-for i in range(25):
-  plt.subplot(5, 5, i+1)
-  plt.title(str(y_train[i]), fontsize=16, color='black', pad=2)
-  plt.imshow(x_train[i], cmap=plt.cm.binary)
-  plt.xticks([])
-  plt.yticks([])
-
-plt.show()
-
 # Spliting test data for validation and testing
 # Here, we will divide test data into validation data and test data so that we can use validation data to
 # avoid overfitting and can use testing data to test the performance of our CNN model. Here we have used
@@ -72,57 +54,7 @@ train_noisy_images = np.clip(train_noisy_images, 0., 1.)
 val_noisy_images = np.clip(val_noisy_images, 0., 1.)
 test_noisy_images = np.clip(test_noisy_images, 0., 1.)
 
-# Visualizing images after adding noise
-
-plt.figure(figsize=(8, 8))
-
-for i in range(25):
-      plt.subplot(5, 5, i+1)
-      plt.title(str(y_train[i]), fontsize=16, color='black', pad=2)
-      plt.imshow(train_noisy_images[i].reshape(1, 28, 28)[0], cmap=plt.cm.binary)
-      plt.xticks([])
-      plt.yticks([])
-
-plt.show()
-
 # Defining our autoencoder model
-
-# model = Sequential()
-#
-# # encoder network
-# model.add(Conv2D(filters=128, kernel_size=(2, 2), activation='relu', padding='same', input_shape=(28, 28, 1)))
-# model.add(tf.keras.layers.BatchNormalization())
-# model.add(Conv2D(filters=128, kernel_size=(2, 2), activation='relu', padding='same'))
-# model.add(tf.keras.layers.BatchNormalization())
-# model.add(Conv2D(filters=256, kernel_size=(2, 2), strides=(2, 2), activation='relu', padding='same'))
-# model.add(tf.keras.layers.BatchNormalization())
-# model.add(Conv2D(filters=256, kernel_size=(2, 2), activation='relu', padding='same'))
-# model.add(tf.keras.layers.BatchNormalization())
-# model.add(Conv2D(filters=512, kernel_size=(3, 3), activation='relu', padding='same'))
-# model.add(tf.keras.layers.BatchNormalization())
-# model.add(Conv2D(filters=512, kernel_size=(2, 2), strides=(2, 2), activation='relu', padding='same'))
-#
-# # decoder network
-# model.add(Conv2D(filters=512, kernel_size=(2, 2), activation='relu', padding='same'))
-# model.add(tf.keras.layers.Conv2DTranspose(filters=512, kernel_size=(2, 2), strides=(2, 2),
-#                                           activation='relu', padding='same'))
-# model.add(tf.keras.layers.BatchNormalization())
-# model.add(Conv2D(filters=256, kernel_size=(2, 2), activation='relu', padding='same'))
-# model.add(tf.keras.layers.BatchNormalization())
-# model.add(Conv2D(filters=256, kernel_size=(2, 2), activation='relu', padding='same'))
-# model.add(tf.keras.layers.BatchNormalization())
-# model.add(Conv2D(filters=128, kernel_size=(2, 2), activation='relu', padding='same'))
-#
-#
-# model.add(tf.keras.layers.Conv2DTranspose(filters=128, kernel_size=(2, 2), strides=(2, 2),
-#                                           activation='relu', padding='same'))
-# model.add(Conv2D(filters=64, kernel_size=(2, 2), activation='relu', padding='same'))
-# model.add(tf.keras.layers.BatchNormalization())
-#
-# model.add(Conv2D(filters=1, kernel_size=(2, 2), activation='relu', padding='same'))
-#
-# # to get the summary of the model
-# model.summary()
 
 model = Sequential()
 
@@ -176,52 +108,6 @@ validation = (val_noisy_images, val_images)
 history = model.fit(train_noisy_images, train_images, batch_size=batch_size, epochs=epochs,
                     validation_data=validation)
 
-# Model evaluation
-# loss and accuracy curve
-
-plt.subplot(2, 1, 1)
-plt.plot(history.history['loss'], label='loss')
-plt.plot(history.history['val_loss'], label='val_loss')
-plt.legend(loc='best')
-plt.subplot(2, 1, 2)
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label='val_accuracy')
-plt.legend(loc='best')
-plt.show()
-
 # Save model
 model.save('my_model.h5')
 loaded_model = load_model('my_model.h5')
-# Visualizing our predicted images along with real and noised images
-
-plt.figure(figsize=(18, 18))
-for i in range(10, 19):
-    plt.subplot(9, 9, i)
-    if i == 14:
-        plt.title('Clean Images', fontsize=25, color='Green')
-    plt.imshow(test_images[i].reshape(1, 28, 28)[0], cmap=plt.cm.binary)
-plt.gcf()
-plt.savefig('clean_images.png')
-plt.show()
-
-plt.figure(figsize=(18, 18))
-for i in range(10, 19):
-    if i == 15:
-        plt.title('Noisy Images', fontsize=25, color='red')
-    plt.subplot(9, 9, i)
-    plt.imshow(test_noisy_images[i].reshape(1, 28, 28)[0], cmap=plt.cm.binary)
-plt.gcf()
-plt.savefig('noisy_images.png')
-plt.show()
-
-plt.figure(figsize=(18, 18))
-for i in range(10, 19):
-    if i == 15:
-        plt.title('Denoised Images', fontsize=25, color='Blue')
-
-    plt.subplot(9, 9, i)
-    plt.imshow(loaded_model.predict(test_noisy_images[i].reshape(1, 28, 28, 1)).reshape(1, 28, 28)[0],
-               cmap=plt.cm.binary)
-plt.gcf()
-plt.savefig('denoised_images.png')
-plt.show()
